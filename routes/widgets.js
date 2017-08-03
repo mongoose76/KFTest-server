d39d2b533346f388f19a6ae109a220a8762f95d4
Doru Paraschiv <doru@snowglobegames.com>
2017-08-03 17:03:40 +0300

    enable CORS

--- routes/widgets.js
+++ routes/widgets.js
@@ -3,27 +3,29 @@ var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:widget', function(req, res, next) {
+router.get('/:widget', function (req, res, next) {
 
     var widget = req.params.widget;
     var filePath = "db/" + widget + ".txt";
     console.log(filePath);
 
-    fs.exists(filePath, function(exists) {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    fs.exists(filePath, function (exists) {
         if (exists) { // results true
-          fs.readFile(filePath, {encoding: "utf8"}, function(err, data){
-             if (err) {
-                console.log(err);
-                res.send(err);
-             } else {
-                res.send(data);
-             }
-          })
-       } else { // file not found
-          res.send('new widget');
-       }
+            fs.readFile(filePath, { encoding: "utf8" }, function (err, data) {
+                if (err) {
+                    console.log(err);                    
+                    res.send(err);
+                } else {
+                    res.send(data);
+                }
+            })
+        } else { // file not found
+            res.send('new widget');
+        }
     });
-    
+
 });
 
 module.exports = router;
